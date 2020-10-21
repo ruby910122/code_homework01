b608a54e4f0d184af6dfd133a0bec1e1fc5950f1
ruby910122 <71751285+ruby910122@users.noreply.github.com>
2020-10-21 09:57:18 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
 basic.forever(function () {
-	
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(4, 4 - x)
+            basic.pause(200)
+        }
+        for (let x = 0; x <= 4; x++) {
+            led.plot(3, 0 + x)
+            basic.pause(200)
+        }
+        for (let x = 0; x <= 4; x++) {
+            led.plot(2, 4 - x)
+            basic.pause(200)
+        }
+        for (let x = 0; x <= 4; x++) {
+            led.plot(1, 0 + x)
+            basic.pause(200)
+        }
+        for (let x = 0; x <= 4; x++) {
+            led.plot(0, 4 - x)
+            basic.pause(200)
+        }
+        basic.clearScreen()
+    }
 })
